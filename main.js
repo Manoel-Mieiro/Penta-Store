6ac30da9790069166086393c1c65d4a018dd6904
Manoel-Mieiro <102331096+Manoel-Mieiro@users.noreply.github.com>
2023-02-14 14:49:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -31,9 +31,12 @@ let shopItemsData = [
     }
 ];
 
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+
 let generateShop = () => {
     return (shop.innerHTML = shopItemsData.map((x) => {
-        let{id, name, price, desc, img}=x
+        let { id, name, price, desc, img } = x
+        let search = basket.find((x)=> x.id === id) || [];
         return ` 
     <div id=product-id-${id} class="item">
         <img width="220" src="${img}" alt="">
@@ -43,9 +46,9 @@ let generateShop = () => {
             <div class="price-quantity">
                 <h2>${price}</h2>
                 <div class="buttons">
-                    <i class="bi bi-dash-lg"></i>
-                    <div class="quantity">0</div>
-                    <i class="bi bi-plus-lg"></i>
+                    <i onclick = "decrement(${id})" class="bi bi-dash-lg"></i>
+                    <div id=${id} class="quantity">${search.item === undefined? 0: search.item}</div>
+                    <i onclick ="increment(${id})" class="bi bi-plus-lg"></i>
                 </div>
             </div>
         </div>
@@ -57,5 +60,48 @@ let generateShop = () => {
 
 generateShop();
 
+
+let increment = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem);
+
+    if (search === undefined) {
+        basket.push({
+            id: selectedItem,
+            item: 1,
+        });
+
+    }
+    else {
+        search.item += 1;
+    }
+localStorage.setItem("data", JSON.stringify(basket));
+    update(selectedItem);
+};
+let decrement = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem);
+
+    if (search.item === 0) return;
+    else {
+        search.item -= 1;
+    }
+    localStorage.setItem("data", JSON.stringify(basket));
+    update(selectedItem);
+};
+
+let update = (id) => {
+    let search = basket.find((x) => x.id === id);
+    console.log(search.item);
+    document.getElementById(id).innerHTML = search.item;
+    total();
+};
+
+let total = () => {
+    let cartIcon = document.getElementById("cartAmount");
+    cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
+
+total();
 
 // shift + backstik do teclado = ``shift + backstik do teclado = ``
