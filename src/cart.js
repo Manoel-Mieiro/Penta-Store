432097abf70f0386fc5cd55b967e7ce1acdd4134
Manoel-Mieiro <102331096+Manoel-Mieiro@users.noreply.github.com>
2023-02-15 14:54:37 -0300

    Add files via upload

--- src/cart.js
+++ src/cart.js
@@ -13,14 +13,31 @@ total();
 let generateCartItens = () => {
 if(basket.length !== 0){
     return (ShoppingCart.innerHTML = basket.map((x) => {
-        console.log(x);
         let {id, item} = x;
         let search = shopItemsData.find((y) => y.id === id) || [];
+        let{img, name, price} = search;
         return `
         <div class="cart-item">
-        <img width="100" src=${search.img} alt=""/>
+        <img width="100" src=${img} alt="Imagem"/>
+        <div class ="details">
+
+            <div class="title-price-x">
+                <h4 class="title-price">
+                <p>${name}</p>
+                <p class="cart-item-price">$${price}</p>
+                </h4>
+                <i onclick ="removeItem(${id})" class="bi bi-x-lg"></i>
+            </div>
+
+            <div class="buttons">
+                <i onclick = "decrement(${id})" class="bi bi-dash-lg"></i>
+                <div id=${id} class="quantity">${item}</div>
+                <i onclick ="increment(${id})" class="bi bi-plus-lg"></i>
+            </div>
+
+        <h3>$ ${item}<h3>
         </div>
-        `;
+        `;  
     })
     .join(""));
 }else{
@@ -35,3 +52,80 @@ if(basket.length !== 0){
 };
 
 generateCartItens();
+
+let increment = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem);
+
+    if (search === undefined) {
+        basket.push({
+            id: selectedItem,
+            item: 1,
+        });
+
+    }
+    else {
+        search.item += 1;
+    }
+    generateCartItens();
+    update(selectedItem);
+    localStorage.setItem("data", JSON.stringify(basket));
+};
+let decrement = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem);
+
+    if (search === undefined) return
+
+    else if (search.item === 0) return;
+    else {
+        search.item -= 1;
+    }
+    update(selectedItem);
+    basket = basket.filter((x) => x.item !== 0);
+    generateCartItens();
+    localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let update = (id) => {
+    let search = basket.find((x) => x.id === id);
+    console.log(search.item);
+    document.getElementById(id).innerHTML = search.item;
+    total();
+    TotalAmout();
+};
+
+let removeItem = (id) =>{
+    let selectedItem = id;
+    basket = basket.filter((x) => x.id !== selectedItem);
+    generateCartItens();
+    TotalAmout();
+    total();
+    localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let clearCart = () =>{
+    basket = [];
+    generateCartItens();
+    total();
+    localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let TotalAmout = () =>{
+    if(basket.length !== 0){
+        let amount = basket.map((x) => {
+            let {item, id} = x;
+            let search = shopItemsData.find((y) => y.id === id) || [];
+            return item*search.price;
+        }).reduce((x, y)=>x+y, 0)
+        // console.log(amount);
+        label.innerHTML=`
+        <h2>Total a Pagar: $ ${amount}</h2> 
+        <button class="checkout">Checkout</button>
+        <button onclick="clearCart()" class="removeAll">Esvaziar Carrinho</button>
+        `;
+    }
+    else return;
+};
+
+TotalAmout();
--- src/data.js
+++ src/data.js
@@ -4,27 +4,27 @@ let shopItemsData = [
         name: "Nome1",
         price: 45,
         desc: "Descrição 1",
-        img: "images/face.jpg"
+        img: "images/face.jpg",
     },
     {
         id: "2",
         name: "Nome2",
         price: 45,
         desc: "Descrição 2",
-        img: "images/copa 2.jpg"
+        img: "images/copa 2.jpg",
     },
     {
         id: "3",
         name: "Nome3",
         price: 45,
         desc: "Descrição 3",
-        img: "images/copa 8.jpg"
+        img: "images/copa 8.jpg",
     },
     {
         id: "4",
         name: "Nome4",
         price: 45,
         desc: "Descrição 4",
-        img: "images/copa 4.jpg"
+        img: "images/copa 4.jpg",
     }
 ];
